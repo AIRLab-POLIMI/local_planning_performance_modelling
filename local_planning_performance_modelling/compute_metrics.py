e375b05600738a9f0fd462f6ecbdab981faf0677
Enri2077 <enry2077@gmail.com>
2021-10-21 13:30:01 +0200

    added cpu and memory metrics

--- local_planning_performance_modelling/compute_metrics.py
+++ local_planning_performance_modelling/compute_metrics.py
@@ -7,8 +7,10 @@ import argparse
 import glob
 import multiprocessing
 import os
+import pickle
 import sys
 import traceback
+from collections import defaultdict
 from os import path
 
 import numpy as np
@@ -30,6 +32,11 @@ class TrajectoryLength:
         self.version = 1
 
     def compute(self):
+        # Do not recompute the metric if it was already computed with the same version
+        if not self.recompute_anyway and \
+                f"{self.metric_name}_version" in self.results_df and \
+                self.results_df.iloc[0][f"{self.metric_name}_version"] == self.version:
+            return True
 
         # check required files exist
         if not path.isfile(self.ground_truth_poses_file_path):
@@ -39,11 +46,6 @@ class TrajectoryLength:
         if not path.isfile(self.run_events_file_path):
             print_error(f"{self.metric_name}: run_events file not found {self.run_events_file_path}")
             return False
-
-        # Do not recompute the metric if it was already computed with the same version
-        if not self.recompute_anyway and self.metric_name in self.results_df and f'{self.metric_name}_version' in self.results_df and \
-                self.results_df.iloc[0][f"{self.metric_name}_version"] == self.version:
-            return True
 
         # get waypoint timestamps info from run events
         run_events_df = pd.read_csv(self.run_events_file_path, engine='python', sep=', ')
@@ -87,16 +89,16 @@ class ExecutionTime:
         self.version = 1
 
     def compute(self):
+        # Do not recompute the metric if it was already computed with the same version
+        if not self.recompute_anyway and \
+                f"{self.metric_name}_version" in self.results_df and \
+                self.results_df.iloc[0][f"{self.metric_name}_version"] == self.version:
+            return True
 
         # check required files exist
         if not path.isfile(self.run_events_file_path):
             print_error(f"{self.metric_name}: run_events file not found {self.run_events_file_path}")
             return False
-
-        # Do not recompute the metric if it was already computed with the same version
-        if not self.recompute_anyway and self.metric_name in self.results_df and f'{self.metric_name}_version' in self.results_df and \
-                self.results_df.iloc[0][f"{self.metric_name}_version"] == self.version:
-            return True
 
         # get waypoint timestamps info from run events
         run_events_df = pd.read_csv(self.run_events_file_path, engine='python', sep=', ')
@@ -130,26 +132,89 @@ class SuccessRate:
         self.version = 1
 
     def compute(self):
+        # Do not recompute the metric if it was already computed with the same version
+        if not self.recompute_anyway and \
+                f"{self.metric_name}_version" in self.results_df and \
+                self.results_df.iloc[0][f"{self.metric_name}_version"] == self.version:
+            return True
 
         # check required files exist
         if not path.isfile(self.run_events_file_path):
             print_error(f"{self.metric_name}: run_events file not found {self.run_events_file_path}")
             return False
 
-        # Do not recompute the metric if it was already computed with the same version
-        if not self.recompute_anyway and self.metric_name in self.results_df and f'{self.metric_name}_version' in self.results_df and \
-                self.results_df.iloc[0][f"{self.metric_name}_version"] == self.version:
-            return True
-
         # get waypoint timestamps info from run events
         run_events_df = pd.read_csv(self.run_events_file_path, engine='python', sep=', ')
         navigation_goal_reached_events = run_events_df[run_events_df.event == 'navigation_goal_reached']
-        print(run_events_df)
 
         navigation_goal_reached = len(navigation_goal_reached_events) == 1
 
         self.results_df[f"{self.metric_name}_version"] = [self.version]
         self.results_df[self.metric_name] = [int(navigation_goal_reached)]
+        return True
+
+
+class CpuTimeAndMaxMemoryUsage:
+    def __init__(self, results_df, run_output_folder, recompute_anyway=False):
+        self.results_df = results_df
+        self.ps_snapshots_folder_path = path.join(run_output_folder, "benchmark_data", "ps_snapshots")
+        self.recompute_anyway = recompute_anyway
+        self.metric_name = "cpu_time_and_max_memory"
+        self.version = 2
+
+    def compute(self):
+        # Do not recompute the metric if it was already computed with the same version
+        if not self.recompute_anyway and \
+                f"{self.metric_name}_version" in self.results_df and \
+                self.results_df.iloc[0][f"{self.metric_name}_version"] == self.version:
+            return True
+
+        # check required files exist
+        if not path.isdir(self.ps_snapshots_folder_path):
+            print_error(f"{self.metric_name}: ps_snapshots directory not found {self.ps_snapshots_folder_path}")
+            return False
+
+        ps_snapshot_files_path = path.join(self.ps_snapshots_folder_path, "ps_*.pkl")
+        ps_snapshot_paths_list = sorted(glob.glob(ps_snapshot_files_path))
+        if len(ps_snapshot_paths_list) == 0:
+            print_error(f"{self.metric_name}: ps_snapshot files not found {ps_snapshot_files_path}")
+            return False
+
+        cpu_time_dict = defaultdict(int)
+        max_memory_dict = defaultdict(int)
+
+        for ps_snapshot_path in ps_snapshot_paths_list:
+            try:
+                with open(ps_snapshot_path, 'rb') as ps_snapshot_file:
+                    ps_snapshot = pickle.load(ps_snapshot_file)
+            except (EOFError, pickle.UnpicklingError) as e:
+                print_error(f"{self.metric_name}: Could not load pickled ps snapshot. Error: {type(e)} {e}. Pickle file: {ps_snapshot_path}")
+                continue
+            for process_info in ps_snapshot:
+                process_name = process_info['name']
+                if process_name not in ['gzserver', 'gzclient', 'rviz2', 'local_planning_', ]:  # ignore simulator and rviz to count the robot system memory
+                    cpu_time_dict[process_name] = max(
+                        cpu_time_dict[process_name],
+                        process_info['cpu_times'].user + process_info['cpu_times'].system
+                    )
+                    max_memory_dict[process_name] = max(
+                        max_memory_dict[process_name],
+                        process_info['memory_full_info'].pss
+                    )
+
+        if len(cpu_time_dict) == 0 or len(max_memory_dict) == 0:
+            print_error(f"{self.metric_name}: no data from ps snapshots {ps_snapshot_files_path}")
+            return False
+
+        self.results_df["controller_cpu_time"] = [cpu_time_dict["controller_server"]]
+        self.results_df["planner_cpu_time"] = [cpu_time_dict["planner_server"]]
+        self.results_df["system_cpu_time"] = [sum(cpu_time_dict.values())]
+
+        self.results_df["controller_max_memory"] = [max_memory_dict["controller_server"]]
+        self.results_df["planner_max_memory"] = [max_memory_dict["planner_server"]]
+        self.results_df["system_max_memory"] = [sum(max_memory_dict.values())]
+
+        self.results_df[f"{self.metric_name}_version"] = [self.version]
         return True
 
 
@@ -179,6 +244,7 @@ def compute_run_metrics(run_output_folder, recompute_all_metrics=False):
     else:
         results_df = pd.DataFrame()
 
+    CpuTimeAndMaxMemoryUsage(results_df=results_df, run_output_folder=run_output_folder, recompute_anyway=recompute_all_metrics).compute()
     TrajectoryLength(results_df=results_df, run_output_folder=run_output_folder, recompute_anyway=recompute_all_metrics).compute()
     ExecutionTime(results_df=results_df, run_output_folder=run_output_folder, recompute_anyway=recompute_all_metrics).compute()
     SuccessRate(results_df=results_df, run_output_folder=run_output_folder, recompute_anyway=recompute_all_metrics).compute()
