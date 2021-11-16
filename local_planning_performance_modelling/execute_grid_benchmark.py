d18faba87e318cee2ca3dc65bebcc6a564bf91c6
Enri2077 <enry2077@gmail.com>
2021-11-16 13:34:57 +0100

    updated grid executor

--- local_planning_performance_modelling/execute_grid_benchmark.py
+++ local_planning_performance_modelling/execute_grid_benchmark.py
@@ -3,31 +3,41 @@
 
 from __future__ import print_function
 
-import glob
 import argparse
 import os
 import sys
 from os import path
 
 from local_planning_performance_modelling.local_planning_benchmark_run import BenchmarkRun
-from performance_modelling_py.benchmark_execution.grid_benchmarking import execute_grid_benchmark
+from performance_modelling_py.benchmark_execution.grid_executor import grid_executor
 from performance_modelling_py.utils import print_error
 
 
 def main():
 
     parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter, description='Execute the benchmark')
-    default_environment_dataset_folders = "~/ds/performance_modelling/test_datasets/dataset/*"
-    parser.add_argument('-e', dest='environment_dataset_folders',
-                        help=f'Dataset folders containing the environment data. Use wildcards to select multiple folders. Only folders are selected, files are ignored. Defaults to {default_environment_dataset_folders}',
+
+    grid_configurations_dir_path = path.expanduser("~/w/ros2_ws/src/local_planning_performance_modelling/config/benchmark_configurations/")
+    default_grid_configuration_from_package = "grid_benchmark_all.yaml"
+    parser.add_argument('-p', dest='grid_configuration_from_package',
+                        help=f'Yaml file with the combinatorial parameters relative to the config/benchmark_configurations folder in this package. If set, the option "grid_configuration" will be ignored.'
+                             f' Defaults to {default_grid_configuration_from_package}. Available configuration files:\n' +
+                             '\n'.join(sorted(os.listdir(grid_configurations_dir_path))),
                         type=str,
-                        default=default_environment_dataset_folders,
+                        default=None,
+                        required=False)
+
+    default_grid_configuration = path.join(grid_configurations_dir_path, default_grid_configuration_from_package)
+    parser.add_argument('-c', dest='grid_configuration',
+                        help=f'Yaml file with the configuration of the benchmark. This option is ignored if the option grid_configuration_from_package is set. Defaults to {default_grid_configuration}.',
+                        type=str,
+                        default=default_grid_configuration,
                         required=False)
 
     benchmark_configurations_dir_path = path.expanduser("~/w/ros2_ws/src/local_planning_performance_modelling/config/benchmark_configurations/")
-    default_benchmark_configuration_from_package = "grid_benchmark_all.yaml"
+    default_benchmark_configuration_from_package = "default.yaml"
     parser.add_argument('-p', dest='benchmark_configuration_from_package',
-                        help=f'Yaml file with the configuration of the benchmark relative to the config/benchmark_configurations folder in this package. If set, the option "benchmark_configuration" will be ignored.'
+                        help=f'Yaml file with the combinatorial parameters relative to the config/benchmark_configurations folder in this package. If set, the option "benchmark_configuration" will be ignored.'
                              f' Defaults to {default_benchmark_configuration_from_package}. Available configuration files:\n' +
                              '\n'.join(sorted(os.listdir(benchmark_configurations_dir_path))),
                         type=str,
@@ -82,20 +92,28 @@ def main():
 
     args = parser.parse_args()
     base_run_folder = path.expanduser(args.base_run_folder)
-    environment_folders = sorted(filter(path.isdir, glob.glob(path.expanduser(args.environment_dataset_folders))))
-    if args.benchmark_configuration_from_package is not None:
-        grid_benchmark_configuration = path.join(benchmark_configurations_dir_path, args.benchmark_configuration_from_package)
+
+    if args.grid_configuration_from_package is not None:
+        grid_configuration = path.join(grid_configurations_dir_path, args.grid_configuration_from_package)
     else:
-        grid_benchmark_configuration = path.expanduser(args.benchmark_configuration)
-    if not path.exists(grid_benchmark_configuration):
-        print_error(f"benchmark configuration file [{path.abspath(grid_benchmark_configuration)}] does not exist")
+        grid_configuration = path.expanduser(args.grid_configuration)
+    if not path.exists(grid_configuration):
+        print_error(f"grid configuration file [{path.abspath(grid_configuration)}] does not exist")
         sys.exit(-1)
 
-    execute_grid_benchmark(benchmark_run_object=BenchmarkRun,
-                           grid_benchmark_configuration=grid_benchmark_configuration,
-                           environment_folders=environment_folders,
-                           base_run_folder=base_run_folder,
-                           num_runs=args.num_runs,
-                           ignore_executed_params_combinations=args.ignore_previous_runs,
-                           shuffle=not args.no_shuffle,
-                           args_parser=args)
+    if args.benchmark_configuration_from_package is not None:
+        benchmark_configuration = path.join(benchmark_configurations_dir_path, args.benchmark_configuration_from_package)
+    else:
+        benchmark_configuration = path.expanduser(args.benchmark_configuration)
+    if not path.exists(benchmark_configuration):
+        print_error(f"benchmark configuration file [{path.abspath(benchmark_configuration)}] does not exist")
+        sys.exit(-1)
+
+    grid_executor(benchmark_run_object=BenchmarkRun,
+                  grid_configuration=grid_configuration,
+                  benchmark_configuration=benchmark_configuration,
+                  base_run_folder=base_run_folder,
+                  num_runs=args.num_runs,
+                  ignore_executed_params_combinations=args.ignore_previous_runs,
+                  shuffle=not args.no_shuffle,
+                  args_parser=args)
