7ee08b441e209345bc5a414d374199accd91c305
Enri2077 <enry2077@gmail.com>
2022-01-26 12:10:14 +0100

    loc generator with mix of normal distributions

--- local_planning_performance_modelling/localization_generator.py
+++ local_planning_performance_modelling/localization_generator.py
@@ -7,6 +7,7 @@ import numpy as np
 import pyquaternion
 import rclpy
 import tf2_ros
+import visualization_msgs.msg
 from rclpy.duration import Duration
 from tf2_ros import TransformBroadcaster
 from rclpy.node import Node
@@ -14,7 +15,9 @@ from rclpy.time import Time
 from rclpy.qos import qos_profile_sensor_data
 import nav_msgs
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import Pose, Quaternion, PoseWithCovarianceStamped, TransformStamped, Pose2D, PoseStamped, PoseArray
+from geometry_msgs.msg import Pose, Quaternion, PoseWithCovarianceStamped, TransformStamped, Pose2D, PoseStamped, PoseArray, Point
+from visualization_msgs.msg import Marker
+
 
 from performance_modelling_py.utils import nanoseconds_to_seconds, print_error, print_info
 
@@ -44,6 +47,12 @@ def normalize_angle_difference(a_angle, b_angle):
     return np.abs(np.arctan2(np.sin(b_angle - a_angle), np.cos(b_angle - a_angle)))
 
 
+def weighted_angle_average(a_angle, a_w, b_angle, b_w):
+    if a_w + b_w == 0.:
+        raise ZeroDivisionError
+    return np.arctan2(a_w * np.sin(a_angle) + b_w * np.sin(b_angle), a_w * np.cos(a_angle) + b_w * np.cos(b_angle))
+
+
 class LocalizationGenerator(Node):
     def __init__(self):
         super().__init__('localization_generator_node', automatically_declare_parameters_from_overrides=True)
@@ -51,8 +60,8 @@ class LocalizationGenerator(Node):
         # topics, services, actions, entities and frames names
         publish_tf_rate = self.get_parameter('publish_tf_rate').value
         update_pose_rate = self.get_parameter('update_pose_rate').value
-        self.translation_error = self.get_parameter('translation_error').value
-        self.rotation_error = self.get_parameter('rotation_error').value
+        self.absolute_translation_error = self.get_parameter('translation_error').value
+        self.absolute_rotation_error = self.get_parameter('rotation_error').value
         self.normalized_relative_translation_error = self.get_parameter('normalized_relative_translation_error').value
         self.normalized_relative_rotation_error = self.get_parameter('normalized_relative_rotation_error').value
         generated_pose_topic = self.get_parameter('generated_pose_topic').value
@@ -78,6 +87,7 @@ class LocalizationGenerator(Node):
         self.last_gt_time = None
         self.trajectory_translation_sum = 0.
         self.trajectory_rotation_sum = 0.
+        self.viz_circles_id = 0
 
         # setup timers
         self.create_timer(1/publish_tf_rate, self.publish_tf_timer_callback)
@@ -93,14 +103,15 @@ class LocalizationGenerator(Node):
         self.gt_poses_publisher = self.create_publisher(PoseArray, "/gt_poses", qos_profile_sensor_data)
         self.rel_poses_publisher = self.create_publisher(PoseArray, "/rel_poses", qos_profile_sensor_data)
         self.gen_poses_publisher = self.create_publisher(PoseArray, "/gen_poses", qos_profile_sensor_data)
+        self.viz_marker_publisher = self.create_publisher(Marker, "/viz_abs", qos_profile_sensor_data)
 
         # setup subscribers
         self.create_subscription(Odometry, ground_truth_pose_topic, self.ground_truth_pose_callback, qos_profile_sensor_data)
 
         self.get_logger().info(f"localization parameters:\n"
                                f"    update_pose_rate: {update_pose_rate}\n"
-                               f"    absolute_translation_error: {self.translation_error}\n"
-                               f"    absolute_rotation_error: {self.rotation_error}\n"
+                               f"    absolute_translation_error: {self.absolute_translation_error}\n"
+                               f"    absolute_rotation_error: {self.absolute_rotation_error}\n"
                                f"    normalized_relative_translation_error: {self.normalized_relative_translation_error}\n"
                                f"    normalized_relative_rotation_error: {self.normalized_relative_rotation_error}"
                                )
@@ -115,33 +126,58 @@ class LocalizationGenerator(Node):
         if now - self.last_gt_time > self.gt_timeout:
             print_error(f"LocalizationGenerator: last ground truth message older than {self.gt_timeout} seconds [{now - self.last_gt_time}]")
 
+        print_info("", logger=self.get_logger().info)
+
         prev_pose_2d_gen = copy(self.pose_2d_gen)
         curr_pose_2d_gt = copy(self.pose_2d_gt)
+        rel_gt_ht = np.linalg.inv(ht(self.last_update_pose_2d_gt)) @ ht(curr_pose_2d_gt)
+        self.publish_gt_poses(curr_pose_2d_gt)
 
+        print_info("self.trajectory_rotation_sum ", self.trajectory_rotation_sum, logger=self.get_logger().info)
         relative_translation_error = self.normalized_relative_translation_error * self.trajectory_translation_sum
         relative_rotation_error = self.normalized_relative_rotation_error * self.trajectory_rotation_sum
-        rel_gt_ht = np.linalg.inv(ht(self.last_update_pose_2d_gt)) @ ht(curr_pose_2d_gt)
+        self.trajectory_translation_sum = 0.
+        self.trajectory_rotation_sum = 0.
+
         rel_error_sample_pose_2d = Pose2D(
             x=random.normalvariate(0., relative_translation_error),
             y=random.normalvariate(0., relative_translation_error),
-            theta=random.normalvariate(0., relative_rotation_error))
+            theta=random.normalvariate(0., relative_rotation_error)
+        )
         rel_ht_with_error = rel_gt_ht @ ht(rel_error_sample_pose_2d)
-        curr_pose_2d_rel = pose_2d(ht(self.prev_pose_2d_rel) @ rel_ht_with_error)
+        curr_pose_2d_rel_with_error = pose_2d(ht(self.prev_pose_2d_rel) @ rel_ht_with_error)
+        self.publish_rel_poses(curr_pose_2d_rel_with_error)
+        self.prev_pose_2d_rel = copy(curr_pose_2d_rel_with_error)
 
-        self.pose_2d_gen.x = random.normalvariate(curr_pose_2d_gt.x, self.translation_error)
-        self.pose_2d_gen.y = random.normalvariate(curr_pose_2d_gt.y, self.translation_error)
-        self.pose_2d_gen.theta = random.normalvariate(curr_pose_2d_gt.theta, self.rotation_error)
+        pose_2d_abs = Pose2D()
+        pose_2d_abs.x = random.normalvariate(curr_pose_2d_gt.x, self.absolute_translation_error)
+        pose_2d_abs.y = random.normalvariate(curr_pose_2d_gt.y, self.absolute_translation_error)
+        pose_2d_abs.theta = random.normalvariate(curr_pose_2d_gt.theta, self.absolute_rotation_error)
 
-        # mix of two normal dists
-        # s_t = np.sqrt((s_a ** 2 * s_r ** 2) / (s_a ** 2 + s_r ** 2))
-        # x_t = (x_r * s_a ** 2 + x_a * s_r ** 2) / (s_a ** 2 + s_r ** 2)
-        # y_t = (y_r * s_a ** 2 + y_a * s_r ** 2) / (s_a ** 2 + s_r ** 2)
+        # product of two normal pdf
+        #   s_t = np.sqrt((s_a**2 * s_r**2) / (s_a**2 + s_r**2))
+        #   x_t = (x_r * s_a**2 + x_a * s_r**2) / (s_a**2 + s_r**2)
+        curr_pose_2d_rel = pose_2d(ht(prev_pose_2d_gen) @ rel_gt_ht)
+        s_tra = np.sqrt((self.absolute_translation_error ** 2 * relative_translation_error ** 2) / (self.absolute_translation_error ** 2 + relative_translation_error ** 2))
+        x_tra = (curr_pose_2d_rel.x * self.absolute_translation_error ** 2 + curr_pose_2d_gt.x * relative_translation_error ** 2) / (self.absolute_translation_error ** 2 + relative_translation_error ** 2)
+        y_tra = (curr_pose_2d_rel.y * self.absolute_translation_error ** 2 + curr_pose_2d_gt.y * relative_translation_error ** 2) / (self.absolute_translation_error ** 2 + relative_translation_error ** 2)
+        s_rot = np.sqrt((self.absolute_rotation_error ** 2 * relative_rotation_error ** 2) / (self.absolute_rotation_error ** 2 + relative_rotation_error ** 2))
+        x_rot = weighted_angle_average(curr_pose_2d_rel.theta, self.absolute_rotation_error ** 2 / (self.absolute_rotation_error ** 2 + relative_rotation_error ** 2),
+                                       curr_pose_2d_gt.theta, relative_rotation_error ** 2 / (self.absolute_rotation_error ** 2 + relative_rotation_error ** 2)
+                                       )
 
-        print_info("", logger=self.get_logger().info)
-        print_info("self.trajectory_translation_sum   ", self.trajectory_translation_sum, logger=self.get_logger().info)
-        print_info("self.trajectory_rotation_sum   ", self.trajectory_rotation_sum, logger=self.get_logger().info)
-        print_info("curr_pose_2d_rel   ", curr_pose_2d_rel, logger=self.get_logger().info)
-        print_info("curr_pose_2d_rel   ", curr_pose_2d_rel, logger=self.get_logger().info)
+        self.publish_viz_circle(curr_pose_2d_rel, .5, "rel")
+        self.publish_viz_circle(curr_pose_2d_gt, .5, "abs")
+
+        self.pose_2d_gen = Pose2D()
+        self.pose_2d_gen.x = random.normalvariate(x_tra, s_tra)
+        self.pose_2d_gen.y = random.normalvariate(y_tra, s_tra)
+        self.pose_2d_gen.theta = random.normalvariate(x_rot, s_rot)
+        self.publish_gen_poses(self.pose_2d_gen)
+
+        # print_info("curr_pose_2d_rel.theta\n\t", curr_pose_2d_rel.theta, logger=self.get_logger().info)
+        # print_info("curr_pose_2d_gt.theta\n\t", curr_pose_2d_gt.theta, logger=self.get_logger().info)
+        # print_info("x_rot\n\t", x_rot, logger=self.get_logger().info)
 
         odom_to_base_tf = self.tf_buffer.lookup_transform(source_frame=self.robot_base_frame, target_frame=self.odom_frame, time=Time())
         r = odom_to_base_tf.transform.rotation
@@ -152,16 +188,8 @@ class LocalizationGenerator(Node):
         )
         self.map_to_odom_pose_2d = pose_2d(ht(self.pose_2d_gen) @ np.linalg.inv(ht(odom_to_base_pose_2d)))  # map_to_odom = map->base ⊕ base->odom
 
-        self.publish_gt_poses(curr_pose_2d_gt)
-        self.publish_rel_poses(curr_pose_2d_rel)
-        self.publish_gen_poses(self.pose_2d_gen)
         self.publish_pose()
-
-        self.trajectory_translation_sum = 0.
-        self.trajectory_rotation_sum = 0.
-
         self.last_update_pose_2d_gt = copy(curr_pose_2d_gt)
-        self.prev_pose_2d_rel = copy(curr_pose_2d_rel)
 
     def ground_truth_pose_callback(self, odometry_msg: nav_msgs.msg.Odometry):
         self.last_gt_time = nanoseconds_to_seconds(Time.from_msg(odometry_msg.header.stamp).nanoseconds)
@@ -186,6 +214,28 @@ class LocalizationGenerator(Node):
         map_to_odom_q = pyquaternion.Quaternion(axis=[0, 0, 1], radians=self.map_to_odom_pose_2d.theta)
         map_to_odom_tf.transform.rotation = Quaternion(w=map_to_odom_q.w, x=map_to_odom_q.x, y=map_to_odom_q.y, z=map_to_odom_q.z)
         self.br.sendTransform(map_to_odom_tf)
+
+    def publish_viz_circle(self, pose_2d_msg, r, ns):
+        pose_msg = Pose()
+        pose_msg.position.x = pose_2d_msg.x
+        pose_msg.position.y = pose_2d_msg.y
+        pose_q = pyquaternion.Quaternion(axis=[0, 0, 1], radians=pose_2d_msg.theta)
+        pose_msg.orientation = Quaternion(w=pose_q.w, x=pose_q.x, y=pose_q.y, z=pose_q.z)
+
+        marker = Marker()
+        marker.header.frame_id = self.fixed_frame
+        marker.header.stamp = self.get_clock().now().to_msg()
+        marker.ns = ns
+        marker.id = self.viz_circles_id
+        marker.type = visualization_msgs.msg.Marker.LINE_STRIP
+        marker.action = visualization_msgs.msg.Marker.ADD
+        marker.points = [Point(x=r*np.cos(t), y=r*np.sin(t)) for t in np.linspace(0, 2*np.pi, 50)]
+        marker.pose = pose_msg
+        marker.scale.x = 0.01
+        marker.color.a = 1.0
+        marker.color.r = 1.0
+        self.viz_marker_publisher.publish(marker)
+        self.viz_circles_id += 1
 
     def publish_pose(self):
         p = PoseWithCovarianceStamped()
