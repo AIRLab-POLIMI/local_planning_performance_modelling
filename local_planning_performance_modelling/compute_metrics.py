22e3c07b65303f633190e976e8300cff367e4dd6
Enri2077 <enry2077@gmail.com>
2022-03-29 18:13:55 +0200

    updated benchmark config and metrics

--- local_planning_performance_modelling/compute_metrics.py
+++ local_planning_performance_modelling/compute_metrics.py
@@ -17,7 +17,7 @@ import yaml
 from yaml.constructor import ConstructorError
 
 from performance_modelling_py.utils import print_info, print_error
-from local_planning_performance_modelling.metrics import CpuTimeAndMaxMemoryUsage, TrajectoryLength, ExecutionTime, SuccessRate, OdometryError, LocalizationError, LocalizationUpdateRate, CollisionRate, Clearance, MotionCharacteristics
+from local_planning_performance_modelling.metrics import CpuTimeAndMaxMemoryUsage, TrajectoryLength, ExecutionTime, SuccessRate, OdometryError, LocalizationError, CollisionlessLocalizationError, LocalizationUpdateRate, CollisionRate, Clearance, MotionCharacteristics
 
 
 def compute_run_metrics(run_output_folder):
@@ -47,16 +47,17 @@ def compute_run_metrics(run_output_folder):
 
     # compute metrics
     metrics_to_compute = [
-        CpuTimeAndMaxMemoryUsage(results_df=results_df, run_output_folder=run_output_folder, recompute_anyway=recompute_all_metrics, verbose=not silent),
+        CollisionRate(results_df=results_df, run_output_folder=run_output_folder, recompute_anyway=recompute_all_metrics, verbose=not silent),
+        Clearance(results_df=results_df, run_output_folder=run_output_folder, recompute_anyway=recompute_all_metrics, verbose=not silent),
         TrajectoryLength(results_df=results_df, run_output_folder=run_output_folder, recompute_anyway=recompute_all_metrics, verbose=not silent),
         ExecutionTime(results_df=results_df, run_output_folder=run_output_folder, recompute_anyway=recompute_all_metrics, verbose=not silent),
         SuccessRate(results_df=results_df, run_output_folder=run_output_folder, recompute_anyway=recompute_all_metrics, verbose=not silent),
-        CollisionRate(results_df=results_df, run_output_folder=run_output_folder, recompute_anyway=recompute_all_metrics, verbose=not silent),
-        Clearance(results_df=results_df, run_output_folder=run_output_folder, recompute_anyway=recompute_all_metrics, verbose=not silent),
         OdometryError(results_df=results_df, run_output_folder=run_output_folder, recompute_anyway=recompute_all_metrics, verbose=not silent),
         LocalizationError(results_df=results_df, run_output_folder=run_output_folder, recompute_anyway=recompute_all_metrics, verbose=not silent),
+        CollisionlessLocalizationError(results_df=results_df, run_output_folder=run_output_folder, recompute_anyway=recompute_all_metrics, verbose=not silent),
         LocalizationUpdateRate(results_df=results_df, run_output_folder=run_output_folder, recompute_anyway=recompute_all_metrics, verbose=not silent),
         MotionCharacteristics(results_df=results_df, run_output_folder=run_output_folder, recompute_anyway=recompute_all_metrics, verbose=not silent),
+        CpuTimeAndMaxMemoryUsage(results_df=results_df, run_output_folder=run_output_folder, recompute_anyway=recompute_all_metrics, verbose=not silent),
     ]
 
     success = True
--- local_planning_performance_modelling/metrics.py
+++ local_planning_performance_modelling/metrics.py
@@ -269,7 +269,7 @@ class CollisionRate:
         self.recompute_anyway = recompute_anyway
         self.verbose = verbose
         self.metric_name = "collision_rate"
-        self.version = 2
+        self.version = 3
 
     def compute(self):
         # Do not recompute the metric if it was already computed with the same version
@@ -292,7 +292,8 @@ class CollisionRate:
             return False
 
         # clear fields in case the computation fails so that the old data (from a previous version) will be removed
-        self.results_df[self.metric_name] = [np.nan]
+        self.results_df['collision_rate'] = [np.nan]
+        self.results_df['collision_time'] = [np.nan]
 
         with open(self.local_costmap_params_path) as local_costmap_params_file:
             local_costmap_params = yaml.safe_load(local_costmap_params_file)
@@ -317,6 +318,7 @@ class CollisionRate:
         scans_df = pd.read_csv(self.scans_file_path, engine='python', sep=', ')
 
         collision = False
+        collision_time = np.nan
         for i, scan_row in scans_df.iterrows():
             laser_scan_msg = LaserScan()
             laser_scan_msg.angle_min = float(scan_row[1])
@@ -332,12 +334,14 @@ class CollisionRate:
                     point = shp.Point(point_pc2[0] + base_scan_x_offset, point_pc2[1] + base_scan_y_offset)
                     if footprint_polygon.contains(point):
                         collision = True
+                        collision_time = float(scan_row[0])
                         break
             if collision:
                 break
 
         self.results_df[f"{self.metric_name}_version"] = [self.version]
-        self.results_df[self.metric_name] = [int(collision)]
+        self.results_df['collision_rate'] = [int(collision)]
+        self.results_df['collision_time'] = [float(collision_time)]
         return True
 
 
@@ -831,6 +835,174 @@ class LocalizationError:
         return True
 
 
+class CollisionlessLocalizationError:
+    def __init__(self, results_df, run_output_folder, recompute_anyway=False, verbose=True):
+        self.results_df = results_df
+        self.ground_truth_poses_file_path = path.join(run_output_folder, "benchmark_data", "ground_truth_poses.csv")
+        self.localization_update_poses_file_path = path.join(run_output_folder, "benchmark_data", "estimated_correction_poses.csv")
+        self.run_events_file_path = path.join(run_output_folder, "benchmark_data", "run_events.csv")
+        self.recompute_anyway = recompute_anyway
+        self.verbose = verbose
+        self.metric_name = "collisionless_localization_update_error"
+        self.version = 1
+
+    def compute(self):
+        # Do not recompute the metric if it was already computed with the same version
+        if not self.recompute_anyway and \
+                f"{self.metric_name}_version" in self.results_df and \
+                self.results_df.iloc[0][f"{self.metric_name}_version"] == self.version:
+            return True
+
+        if 'collision_rate' not in self.results_df or np.isnan(self.results_df['collision_rate'].iloc[0]):
+            print_error(f"{self.metric_name}: collision_rate result not found or nan")
+            return False
+
+        # clear fields in case the computation fails so that the old data (from a previous version) will be removed
+        self.results_df["collisionless_localization_update_absolute_translation_error_mean"] = [np.nan]
+        self.results_df["collisionless_localization_update_absolute_translation_error_std"] = [np.nan]
+        self.results_df["collisionless_localization_update_absolute_rotation_error_mean"] = [np.nan]
+        self.results_df["collisionless_localization_update_absolute_rotation_error_std"] = [np.nan]
+        self.results_df["collisionless_localization_update_relative_translation_error_mean"] = [np.nan]
+        self.results_df["collisionless_localization_update_relative_translation_error_std"] = [np.nan]
+        self.results_df["collisionless_localization_update_relative_rotation_error_mean"] = [np.nan]
+        self.results_df["collisionless_localization_update_relative_rotation_error_std"] = [np.nan]
+        self.results_df["collisionless_localization_update_normalized_relative_translation_error_mean"] = [np.nan]
+        self.results_df["collisionless_localization_update_normalized_relative_translation_error_std"] = [np.nan]
+        self.results_df["collisionless_localization_update_normalized_relative_rotation_error_mean"] = [np.nan]
+        self.results_df["collisionless_localization_update_normalized_relative_rotation_error_std"] = [np.nan]
+
+        # check required files exist
+        if not path.isfile(self.ground_truth_poses_file_path):
+            print_error(f"{self.metric_name}: ground_truth_poses file not found:\n{self.ground_truth_poses_file_path}")
+            return False
+
+        if not path.isfile(self.localization_update_poses_file_path):
+            print_error(f"{self.metric_name}: estimated_localization_update_poses file not found:\n{self.localization_update_poses_file_path}")
+            return False
+
+        if not path.isfile(self.run_events_file_path):
+            print_error(f"{self.metric_name}: run_events file not found:\n{self.run_events_file_path}")
+            return False
+
+        # get timestamps info from run events
+        run_events_df = pd.read_csv(self.run_events_file_path, engine='python', sep=', ')
+        navigation_start_events = run_events_df[run_events_df.event == 'navigation_goal_accepted']
+        navigation_succeeded_events = run_events_df[(run_events_df.event == 'navigation_succeeded')]
+        navigation_failed_events = run_events_df[(run_events_df.event == 'navigation_failed')]
+        collision_time = self.results_df['collision_time'].iloc[0] if self.results_df['collision_rate'].iloc[0] == 1 else None
+
+        if len(navigation_start_events) != 1:
+            print_info(f"{self.metric_name}: event navigation_goal_accepted not in events file:\n{self.run_events_file_path}")
+            self.results_df[f"{self.metric_name}_version"] = [self.version]
+            return True
+
+        if len(navigation_succeeded_events) + len(navigation_failed_events) != 1:
+            print_info(f"{self.metric_name}: events navigation_succeeded and navigation_failed not in events file:\n{self.run_events_file_path}")
+            self.results_df[f"{self.metric_name}_version"] = [self.version]
+            return True
+
+        navigation_start_time = navigation_start_events.iloc[0].t
+        navigation_end_time = navigation_succeeded_events.iloc[0].t if len(navigation_succeeded_events) == 1 else navigation_failed_events.iloc[0].t
+        if collision_time is not None:
+            navigation_end_time = min(navigation_end_time, collision_time)
+
+        # get the dataframes for ground truth poses
+        ground_truth_poses_df = pd.read_csv(self.ground_truth_poses_file_path)
+        ground_truth_poses_df = ground_truth_poses_df[(navigation_start_time <= ground_truth_poses_df.t) & (ground_truth_poses_df.t <= navigation_end_time)]
+
+        # get the dataframes for localization update poses
+        localization_update_poses_df = pd.read_csv(self.localization_update_poses_file_path)
+        if len(localization_update_poses_df) == 0:
+            if self.verbose:
+                if self.verbose:
+                    print_info(f"{self.metric_name}: not enough localization update poses in navigation interval [{navigation_start_time}, {navigation_end_time}]:\n{self.localization_update_poses_file_path}")
+            self.results_df[f"{self.metric_name}_version"] = [self.version]
+            return True
+        localization_update_poses_df = localization_update_poses_df[(navigation_start_time <= localization_update_poses_df.t) & (localization_update_poses_df.t <= navigation_end_time)]
+
+        # compute the interpolated ground truth poses
+        try:
+            interpolated_df = interpolate_pose_2d_trajectories(
+                trajectory_a_df=localization_update_poses_df, trajectory_a_label='est',
+                trajectory_b_df=ground_truth_poses_df, trajectory_b_label='gt',
+                limit_trajectory_a_rate=False, interpolation_tolerance=0.1
+            )
+        except InterpolationException as e:
+            print_info(f"{self.metric_name}: interpolation exception: {e} when interpolating:\n{self.ground_truth_poses_file_path}")
+            self.results_df[f"{self.metric_name}_version"] = [self.version]
+            return True
+
+        # if not enough matching ground truth data points are found, the metrics can not be computed
+        if len(interpolated_df.index) < 2:
+            print_info(f"{self.metric_name}: no matching ground truth data points were found when interpolating:\n{self.ground_truth_poses_file_path}")
+            self.results_df[f"{self.metric_name}_version"] = [self.version]
+            return True
+
+        interpolated_poses = interpolated_df[['t', 'x_est', 'y_est', 'theta_est', 'x_gt', 'y_gt', 'theta_gt']].values
+
+        absolute_translation_errors = list()
+        absolute_rotation_errors = list()
+        relative_translation_errors = list()
+        relative_rotation_errors = list()
+        normalized_relative_translation_errors = list()
+        normalized_relative_rotation_errors = list()
+        for i in range(1, len(interpolated_df)):
+            start_timestamp = interpolated_poses[i - 1, 0]
+            end_timestamp = interpolated_poses[i, 0]
+
+            # compute ground truth and estimate transforms from start to end
+            ground_truth_start_pose = interpolated_poses[i - 1, 4:7]
+            x_gt, y_gt, theta_gt = ground_truth_end_pose = interpolated_poses[i, 4:7]
+            est_start_pose = interpolated_poses[i - 1, 1:4]
+            x_est, y_est, theta_est = est_end_pose = interpolated_poses[i, 1:4]
+
+            # compute absolute error
+            absolute_translation_error = np.sqrt((x_gt - x_est) ** 2 + (y_gt - y_est) ** 2)
+            absolute_rotation_error = np.abs(np.arctan2(np.sin(theta_gt - theta_est), np.cos(theta_gt - theta_est)))
+
+            # compute relative error
+            relative_error_x, relative_error_y, relative_error_theta = relative_2d_pose_transform(ground_truth_start_pose, ground_truth_end_pose, est_start_pose, est_end_pose)
+            relative_translation_error = np.sqrt(relative_error_x ** 2 + relative_error_y ** 2)
+            relative_rotation_error = np.abs(np.arctan2(np.sin(relative_error_theta), np.cos(relative_error_theta)))
+
+            # compute the integral of translation between updates
+            ground_truth_poses_df_clipped = ground_truth_poses_df[(start_timestamp <= ground_truth_poses_df.t) & (ground_truth_poses_df.t <= end_timestamp)]
+            ground_truth_positions = ground_truth_poses_df_clipped[['x', 'y']].values
+            squared_deltas = (ground_truth_positions[1:-1] - ground_truth_positions[0:-2]) ** 2  # equivalent to (x_2-x_1)**2, (y_2-y_1)**2, for each row
+            trajectory_translation = np.sqrt(np.sum(squared_deltas, axis=1)).sum()  # equivalent to sum for each row of sqrt( (x_2-x_1)**2 + (y_2-y_1)**2 )
+
+            # compute the integral of rotation between updates
+            ground_truth_rotations = ground_truth_poses_df_clipped['theta'].values
+            rotation_differences = ground_truth_rotations[1:-1] - ground_truth_rotations[0:-2]
+            rotation_deltas = np.abs(np.arctan2(np.sin(rotation_differences), np.cos(rotation_differences)))  # normalize the angle difference
+            trajectory_rotation = np.sum(rotation_deltas)
+
+            absolute_translation_errors.append(absolute_translation_error)
+            absolute_rotation_errors.append(absolute_rotation_error)
+            relative_translation_errors.append(relative_translation_error)
+            relative_rotation_errors.append(relative_rotation_error)
+            if trajectory_translation > 0.01:  # only compute this errors if there was a meaningful translation (1cm)
+                normalized_relative_translation_errors.append(relative_translation_error / trajectory_translation)
+            if trajectory_rotation > 0.01:  # only compute this errors if there was a meaningful rotation (0.01rad =~ 0.6deg)
+                normalized_relative_rotation_errors.append(relative_rotation_error/trajectory_rotation)
+
+        self.results_df["collisionless_localization_update_absolute_translation_error_mean"] = [float(np.mean(absolute_translation_errors)) if len(absolute_translation_errors) else np.nan]
+        self.results_df["collisionless_localization_update_absolute_translation_error_std"] = [float(np.std(absolute_translation_errors)) if len(absolute_translation_errors) else np.nan]
+        self.results_df["collisionless_localization_update_absolute_rotation_error_mean"] = [float(np.mean(absolute_rotation_errors)) if len(absolute_rotation_errors) else np.nan]
+        self.results_df["collisionless_localization_update_absolute_rotation_error_std"] = [float(np.std(absolute_rotation_errors)) if len(absolute_rotation_errors) else np.nan]
+        self.results_df["collisionless_localization_update_relative_translation_error_mean"] = [float(np.mean(relative_translation_errors)) if len(relative_translation_errors) else np.nan]
+        self.results_df["collisionless_localization_update_relative_translation_error_std"] = [float(np.std(relative_translation_errors)) if len(relative_translation_errors) else np.nan]
+        self.results_df["collisionless_localization_update_relative_rotation_error_mean"] = [float(np.mean(relative_rotation_errors)) if len(relative_rotation_errors) else np.nan]
+        self.results_df["collisionless_localization_update_relative_rotation_error_std"] = [float(np.std(relative_rotation_errors)) if len(relative_rotation_errors) else np.nan]
+        self.results_df["collisionless_localization_update_normalized_relative_translation_error_mean"] = [float(np.mean(normalized_relative_translation_errors)) if len(normalized_relative_translation_errors) else np.nan]
+        self.results_df["collisionless_localization_update_normalized_relative_translation_error_std"] = [float(np.std(normalized_relative_translation_errors)) if len(normalized_relative_translation_errors) else np.nan]
+        self.results_df["collisionless_localization_update_normalized_relative_rotation_error_mean"] = [float(np.mean(normalized_relative_rotation_errors)) if len(normalized_relative_rotation_errors) else np.nan]
+        self.results_df["collisionless_localization_update_normalized_relative_rotation_error_std"] = [float(np.std(normalized_relative_rotation_errors)) if len(normalized_relative_rotation_errors) else np.nan]
+
+        self.results_df[f"{self.metric_name}_version"] = [self.version]
+        return True
+
+
 class LocalizationUpdateRate:
     def __init__(self, results_df, run_output_folder, recompute_anyway=False, verbose=True):
         self.results_df = results_df
@@ -909,7 +1081,7 @@ class MotionCharacteristics:
         self.recompute_anyway = recompute_anyway
         self.verbose = verbose
         self.metric_name = "motion_characteristics"
-        self.version = 3
+        self.version = 4
 
     def compute(self):
         # Do not recompute the metric if it was already computed with the same version
@@ -967,11 +1139,17 @@ class MotionCharacteristics:
         diff_df['a_rot'] = diff_df.v_theta/diff_df.t
         diff_df = diff_df.rolling(11, center=True).mean()
 
-        self.results_df["average_translation_velocity"] = [float(np.mean(np.abs(ground_truth_poses_df.v_tran)))]
-        self.results_df["average_rotation_velocity"] = [float(np.mean(np.abs(ground_truth_poses_df.v_rot)))]
-        self.results_df["translation_rotation_product"] = [float(np.mean(np.abs(ground_truth_poses_df.v_tran) * np.abs(ground_truth_poses_df.v_rot)))]
-        self.results_df["average_translation_acceleration"] = [float(np.mean(np.abs(diff_df.a_tran)))]
-        self.results_df["average_rotation_acceleration"] = [float(np.mean(np.abs(diff_df.a_rot)))]
-        self.results_df["translation_rotation_acceleration_product"] = [float(np.mean(np.abs(diff_df.a_tran) * np.abs(diff_df.a_rot)))]
+        vt = np.abs(ground_truth_poses_df.v_tran)
+        vr = np.abs(ground_truth_poses_df.v_rot)
+        at = np.abs(diff_df.a_tran)
+        ar = np.abs(diff_df.a_rot)
+
+        self.results_df["average_velocity_atan"] = [float(np.mean(np.arctan2(vr, vt)))]
+        self.results_df["average_translation_velocity"] = [float(np.mean(vt))]
+        self.results_df["average_rotation_velocity"] = [float(np.mean(vr))]
+        self.results_df["translation_rotation_product"] = [float(np.mean(vt * vr))]
+        self.results_df["average_translation_acceleration"] = [float(np.mean(at))]
+        self.results_df["average_rotation_acceleration"] = [float(np.mean(ar))]
+        self.results_df["translation_rotation_acceleration_product"] = [float(np.mean(at * ar))]
         self.results_df[f"{self.metric_name}_version"] = [self.version]
         return True
