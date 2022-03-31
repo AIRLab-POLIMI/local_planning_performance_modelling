c1745dbe58679c4b1413969b6a845e3145d5080c
Enri2077 <enry2077@gmail.com>
2022-03-31 11:56:26 +0200

    added script to install dependences

---
+++ install_dependences.sh
@@ -0,0 +1,9 @@
+sudo apt install -y python-shapely python-termcolor python3-termcolor python-psutil python3-psutil python-pip python-catkin-tools ros-melodic-catkin ros-melodic-gazebo-ros ros-melodic-gazebo-plugins ros-melodic-control-toolbox ros-melodic-controller-manager ros-melodic-transmission-interface ros-melodic-joint-limits-interface ros-melodic-robot-state-publisher ros-melodic-map-server ros-melodic-slam-toolbox ros-melodic-gmapping ros-melodic-hector-slam ros-melodic-amcl ros-melodic-move-base ros-melodic-global-planner ros-melodic-dwa-local-planner ros-melodic-tf-conversions ros-melodic-tf2-sensor-msgs bash-completion htop python3-pip git-extras 
+
+pip install pyquaternion
+pip3 install pyquaternion
+pip install networkx
+pip3 install networkx
+pip install scikit-image
+pip3 install scikit-image
+
